b93b7c908d0be8890582bc698830e1738f075b5b
Ole Kristian Losvik <losvik@gmail.com>
2017-05-07 18:24:15 +0200

    better spacing in sidebar widgets

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ function positor_widgets_init() {
 		'name'          => esc_html__( 'Sidebar', 'positor' ),
 		'id'            => 'sidebar-1',
 		'description'   => '',
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="widget %2$s my-5">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<div class="col-md-3 pt-5">
+<div class="col-md-3">
 	<aside id="secondary" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside>
