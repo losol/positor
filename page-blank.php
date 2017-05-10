ef32e7aff26c6f2b9a962b2db7a4ce271a33e65f
Ole Kristian Losvik <losvik@gmail.com>
2017-05-10 20:42:57 +0200

    added blank page

---
+++ page-blank.php
@@ -0,0 +1,16 @@
+ <?php
+/**
+ * Template Name: Blank
+ * For use with page builders
+ * 
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Positor
+ */
+ ?>
+
+<?php get_header(); ?>
+<div class="container-fluid" id="content">
+</div>
+
+<?php get_footer(); ?>
