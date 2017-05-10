18a8213d5729f580b1962c4603440ab80efae580
Ole Kristian Losvik <losvik@gmail.com>
2017-05-10 20:54:03 +0200

    fix

--- components/page/content-page.php
+++ components/page/content-page.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('fl-post'); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title pt-5">', '</h1>' ); ?>
 	</header>
