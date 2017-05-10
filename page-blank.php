37b3fcd475c5cdb8ded74162a7399606b2a6f0c4
Ole Kristian Losvik <losvik@gmail.com>
2017-05-10 20:48:38 +0200

    fix

--- page-blank.php
+++ page-blank.php
@@ -10,7 +10,20 @@
  ?>
 
 <?php get_header(); ?>
-<div class="container-fluid" id="content">
+<div class="container-fluid fl-content-full" id="content">
+	<div class="row">
+			<main id="main" class="fl-content">
+				<?php
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'components/page/content', 'page' );
+					get_template_part( 'components/common/comments');
+
+				endwhile; // End of the loop.
+				?>
+			</main>
+	</div>
 </div>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
