0231c6bd76289c5806d40aefc9c205f5e99b3d06
Ole Kristian Losvik <losvik@gmail.com>
2017-06-28 18:37:35 +0200

    fix

--- inc/commentform.php
+++ inc/commentform.php
@@ -9,11 +9,11 @@ function bootstrap4_comment_form_fields( $fields ) {
     $html5    = current_theme_supports( 'html5', 'comment-form' ) ? 1 : 0;
     
     $fields   =  array(
-        'author' => '<div class="form-group comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+        'author' => '<div class="form-group comment-form-author">' . '<label for="author">' . __( 'Name' , 'positor') . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
                     '<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></div>',
-        'email'  => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+        'email'  => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email' , 'positor') . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
                     '<input class="form-control" id="email" name="email" ' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></div>',
-        'url'    => '<div class="form-group comment-form-url"><label for="url">' . __( 'Website' ) . '</label> ' .
+        'url'    => '<div class="form-group comment-form-url"><label for="url">' . __( 'Website' , 'positor') . '</label> ' .
                     '<input class="form-control" id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></div>'        
     );
     
@@ -23,7 +23,7 @@ function bootstrap4_comment_form_fields( $fields ) {
 add_filter( 'comment_form_defaults', 'bootstrap4_comment_form' );
 function bootstrap4_comment_form( $args ) {
     $args['comment_field'] = '<div class="form-group comment-form-comment">
-            <label for="comment">' . _x( 'Comment', 'noun' ) . '</label> 
+            <label for="comment">' . _x( 'Comment', 'noun', 'positor' ) . '</label> 
             <textarea class="form-control" id="comment" name="comment" cols="45" rows="6" aria-required="true"></textarea>
         </div>';
     $args['class_submit'] = 'btn btn-default'; // since WP 4.1
