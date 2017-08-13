44bd9f284163a06e1c2f1f5bfdad5ab4659b9aaf
Ole Kristian Losvik <losvik@gmail.com>
2017-08-13 20:58:32 +0200

    added white space

--- components/common/post-navigation.php
+++ components/common/post-navigation.php
@@ -15,7 +15,7 @@
 	$prev_post = get_previous_post();
 	if ( ! empty( $prev_post ) ) : ?>
 
-	<a href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>" rel="prev" title="Navigate to the previous post" class="link-no-decoration btn btn-outline-primary m-1 col-6 d-flex align-items-stretch rounded-0">
+	<a href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>" rel="prev" title="Navigate to the previous post" class="white-space-normal link-no-decoration btn btn-outline-primary m-1 col-6 d-flex align-items-stretch rounded-0">
 	<span class="fa fa-lg fa-chevron-left d-flex align-items-center mx-2" aria-hidden="true"></span> 
 	<?php echo esc_attr( $prev_post->post_title ); ?>
 	</a>
@@ -27,7 +27,7 @@
 	$next_post = get_next_post();
 	if ( ! empty( $next_post ) ) : ?>
 																							
-	<a href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>" rel="next" title="Navigate to the next post" class="link-no-decoration btn btn-outline-primary m-1 col-6 d-flex align-items-stretch flex-row-reverse">
+	<a href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>" rel="next" title="Navigate to the next post" class="white-space-normal link-no-decoration btn btn-outline-primary m-1 col-6 d-flex align-items-stretch flex-row-reverse">
 		<span class="fa fa-lg fa-chevron-right d-flex align-items-center mx-2" aria-hidden="true"></span> 
 		<?php echo esc_attr( $next_post->post_title ); ?>
 	</a>
