d59613909347b90232ae02c564bdb5282e470fd8
Ole Kristian Losvik <losvik@gmail.com>
2017-09-10 21:40:06 +0200

    asdf

--- archive.php
+++ archive.php
@@ -9,47 +9,54 @@
 
 ?>
 <?php get_header(); ?>
-<div class="bg-gray-light py-3">
+<div class="bg-gray-300 py-3">
 			<header class="container page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_title( '<h1 class="page-title display-2">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description lead">', '</div>' );
 				?>
 			</header>
 </div>
-<div class="container flex-grow">
-	<div class="row">
-		<div class="col-md-12">
-			<main id="main" class="site-main" role="main">
+<div class="container">
+<div class="row">
+<!-- Loop -->
+	<?php
+	
+	if ( have_posts() ) :
+		// Check if there should be place for a sidebar.
+		if ( is_active_sidebar( 'sidebar-frontpage' ) ) {
+			echo '<div class="col-md-8">'; 
+		} else {
+			echo '<div class="col">';
+		}
 
-				<?php
-				if ( have_posts() ) : ?>
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
+			echo '<div class="py-3">';
+			get_template_part( 'components/card/card-standard', get_post_format() );
+			echo '</div>';
+		endwhile;
+		echo '</div>';
 
+		// Show sidebar.
+		if ( is_active_sidebar( 'sidebar-frontpage' ) ) {
+			echo '<div class="col-md-4 py-5">';
+			dynamic_sidebar( 'sidebar-frontpage' );
+			echo '</div>';
+		}
 
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
+		// Post navigation for prev/next index page.
+		echo '<div class="container">';
+		get_template_part( 'components/common/archive-navigation', get_post_format() );
+		echo '</div>';
+	else :
 
-						/*
-						* Include the Post-Format-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						*/
-						get_template_part( 'components/card/card-standard', get_post_format() );
+		get_template_part( 'components/post/content', 'none' );
 
-					endwhile;
-
-					the_posts_navigation();
-
-				else :
-
-					get_template_part( 'components/post/content', 'none' );
-
-				endif; ?>
-
-			</main>
-		</div>
-	</div>
+endif; ?>
 </div>
+</div>
+<!-- End of Loop -->
+
 <?php
 get_footer();
