9ab5fd88a539a14748a7f0b0b70ad96465693849
Ole Kristian Losvik <losvik@gmail.com>
2017-09-10 03:31:54 +0200

    fix

--- components/header/navbar-large.php
+++ components/header/navbar-large.php
@@ -49,11 +49,12 @@ if ( ! $hide_navbar ) { ?>
 					);
 				
 					?>
-				</div>
-			
 				<button class="btn btn-link text-light" type="button" data-toggle="collapse" data-target="#navbar-searchform" aria-controls="searchform" aria-expanded="false" aria-label="Toggle navigation">
 					<i class="fa fa-fw fa-1_2x fa-search" aria-hidden="true"></i>&nbsp;<?php esc_html_e( 'Search', 'positor' ); ?>
 				</button>
+				</div>
+			
+
 			
 				
 			
