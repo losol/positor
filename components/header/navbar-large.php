acfbe952ede2fc83b3e139b7a69c49c4e1a7ebe8
Ole Kristian Losvik <losvik@gmail.com>
2017-09-10 03:44:16 +0200

    asdf

--- components/header/navbar-large.php
+++ components/header/navbar-large.php
@@ -19,9 +19,11 @@ if ( ! $hide_navbar ) { ?>
 	<div class="container">
 		<div class="row py-2">
 			<div class="col-12 text-inverse text-center">
+				<div class="w-75 mx-auto">
 				<?php
 					positor_the_custom_logo();
 				?>
+				</div>
 			</div>
 		</div>
 		<div class="row">
@@ -32,14 +34,13 @@ if ( ! $hide_navbar ) { ?>
 					<i class="fa fa-fw fa-1_2x fa-bars " aria-hidden="true"></i>&nbsp;<span><?php esc_html_e( 'Menu', 'positor' ); ?></span>
 				</button>
 			
-				<div id="nav-centered" class="d-none d-lg-block mx-auto text-light">
 				<?php
 					wp_nav_menu( array(
 						'menu'            => 'top',
 						'theme_location'  => 'menu-1',
 						'container'       => 'div',
 						'container_id'    => 'navbar-menu',
-						'container_class' => 'navbar-collapse',
+						'container_class' => 'navbar-collapse d-none d-lg-block mx-auto text-light',
 						'menu_id'         => 'top_menu',
 						'menu_class'      => 'navbar-nav mr-auto',
 						'depth'           => 2,
@@ -49,12 +50,10 @@ if ( ! $hide_navbar ) { ?>
 					);
 				
 					?>
+			
 				<button class="btn btn-link text-light" type="button" data-toggle="collapse" data-target="#navbar-searchform" aria-controls="searchform" aria-expanded="false" aria-label="Toggle navigation">
 					<i class="fa fa-fw fa-1_2x fa-search" aria-hidden="true"></i>&nbsp;<?php esc_html_e( 'Search', 'positor' ); ?>
 				</button>
-				</div>
-			
-
 			
 				
 			
