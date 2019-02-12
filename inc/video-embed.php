46ffcfb998525b37e4041a56f2b46b337931cd3d
Ole Kristian Losvik <losolio@github.user>
2019-02-12 18:34:37 +0100

    Format

--- inc/video-embed.php
+++ inc/video-embed.php
@@ -19,4 +19,4 @@ function bootstrap_wrap_oembed( $html ) {
 	// Wrap in div element and return #3 and #4.
 	return'<div class="embed-responsive embed-responsive-16by9 my-2">' . $html . '</div>';
 }
-add_filter( 'embed_oembed_html','bootstrap_wrap_oembed', 10, 1 );
+add_filter( 'embed_oembed_html', 'bootstrap_wrap_oembed', 10, 1 );
