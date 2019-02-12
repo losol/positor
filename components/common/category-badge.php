eb2e5ce936972f82a46d1e258367863aae7121a0
Ole Kristian Losvik <losolio@github.user>
2019-02-12 18:42:23 +0100

    Added file comment

--- components/common/category-badge.php
+++ components/common/category-badge.php
@@ -1,8 +1,19 @@
-<?php // Show categories badges if has_category, and it is not the "uncategorized" category.
+<?php
+/**
+ * Category badges
+ *
+ * @package Positor
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @since   1.0.0
+ */
+?>
+
+<?php
+	// Show categories badges if has_category, and it is not the "uncategorized" category.
 	$categories = wp_get_post_categories( $post->ID );
-if ( has_category() && ! ( count( $categories ) === 1 && in_array( 1, $categories, true ) ) ) :
-	echo '<div class="py-1"><p>';
-	echo '<span class="sr-only">' . esc_html_e( 'Category: ', 'positor' ) . '</span>' . positor_the_categories(); // WPCS: XSS OK.
-	echo '</p></div>';
+	if ( has_category() && ! ( count( $categories ) === 1 && in_array( 1, $categories, true ) ) ) :
+		echo '<div class="py-1"><p>';
+		echo '<span class="sr-only">' . esc_html_e( 'Category: ', 'positor' ) . '</span>' . positor_the_categories(); // WPCS: XSS OK.
+		echo '</p></div>';
 	endif;
 
