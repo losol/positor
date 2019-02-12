226570b9b16603f7abf549dfa14cc4e62803c3cc
Ole Kristian Losvik <losolio@github.user>
2019-02-12 17:49:05 +0100

    Added phpcbf setting

--- comments.php
+++ comments.php
@@ -5,8 +5,8 @@
  * This is the template that displays the area of the page that contains both
  * the existing comments and the comment form.
  *
- * @package  Positor
- * @link     https://codex.wordpress.org/Template_Hierarchy
+ * @package Positor
+ * @link    https://codex.wordpress.org/Template_Hierarchy
  */
 
 // Check if the post is protected by a password.
@@ -18,25 +18,26 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area my-3 d-print-none">
 
 	<?php
-	if ( have_comments() ) : ?>
+	if ( have_comments() ) :
+		?>
 		<h3 class="comments-title py-3">
-			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'positor' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'positor' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
+		<?php
+		$comment_count = get_comments_number();
+		if ( 1 === $comment_count ) {
+			printf(
+				/* translators: 1: title. */
+				esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'positor' ),
+				'<span>' . get_the_title() . '</span>'
+			);
+		} else {
+			printf( // WPCS: XSS OK.
+				/* translators: 1: comment count number, 2: title. */
+				esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'positor' ) ),
+				number_format_i18n( $comment_count ),
+				'<span>' . get_the_title() . '</span>'
+			);
+		}
+		?>
 		</h3>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
@@ -53,14 +54,16 @@ if ( post_password_required() ) {
 
 		<div class="py-5">
 		<?php
-			wp_list_comments( array(
-				'style'         => 'ol',
-				'max_depth'     => '',
-				'short_ping'    => true,
-				'avatar_size'   => '75',
-				'walker'        => new Bootstrap_Comment_Walker(),
-			) );
-			?>
+		wp_list_comments(
+			array(
+				'style'       => 'ol',
+				'max_depth'   => '',
+				'short_ping'  => true,
+				'avatar_size' => '75',
+				'walker'      => new Bootstrap_Comment_Walker(),
+			)
+		);
+		?>
 
 		</div><!-- .comment-list -->
 
@@ -74,17 +77,18 @@ if ( post_password_required() ) {
 
 			</div>
 		</nav>
-		<?php
+			<?php
 		endif; // Check for comment navigation.
 
 	endif; // Check for have_comments().
 
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
 
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'positor' ); ?></p>
-	<?php
+		<?php
 	endif;
 
 	comment_form();
--- docs/Add_code_linting.md
+++ docs/Add_code_linting.md
@@ -5,8 +5,10 @@ Add javascript linting:
 2. Install `jshint` in VS Code extensions
 
 Adding the WordPress Coding Standards WPCS when working in VS Code (on mac)
-* Install [https://getcomposer.org/doc/00-intro.md](Composer)
+* Install [https://brew.sh/](Brew)
+* Install [https://getcomposer.org/doc/00-intro.md](Composer) with Brew: `brew install composer`
+* Add PHPCS: `composer global require squizlabs/php_codesniffer`
+* Add the latest WordPress Coding Standard (WPCS)  `composer global require wp-coding-standards/wpcs`
 * Install the phpcs extension in VS Code: 'phpcs'
-* Add the latest WordPress Coding Standard (WPCS)  `npm install wpcs --save-dev`
-* In the theme folder run: `composer global require "squizlabs/php_codesniffer=*"`
-* Right click the wpcs folder in your node_Modules foler, replace /path/to/wpcs and run: `~/.composer/vendor/bin/phpcs --config-set installed_paths /path/to/wpcs`
+* Right click the wpcs folder in your node_Modules foler, replace /path/to/wpcs and run: `~/.composer/vendor/bin/phpcs --config-set installed_paths /Users/ole/.composer/vendor/wp-coding-standards/wpcs`
+* Add this line to your VS Code settings: "phpcbf.standard": "WordPress"
